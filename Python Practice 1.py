99fa92cde11c617b11a6804a7d1e7bf66f7b3d8f
serramilli <34144036+serramilli@users.noreply.github.com>
2018-11-02 07:40:12 -0400

    Add files via upload

---
+++ Python Practice 1.py
@@ -0,0 +1,7 @@
+for i in range(0, 10):
+    print("GCI is great")
+
+name = input("Hey! What's your name? ")
+print("Hello " + name + ", pleased to meet you! " +
+    "Did you know that your name backwards is " + name[::-1] +"?")
+
